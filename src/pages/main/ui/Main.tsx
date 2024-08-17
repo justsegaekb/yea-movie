33bbf7d00e76ec0d74fc11827985a3c81326e94f
Serge <justsegaekb@yandex.ru>
2024-08-17 18:05:16 +0500

    add rating component

--- src/pages/main/ui/Main.tsx
+++ src/pages/main/ui/Main.tsx
@@ -9,23 +9,28 @@ import { Rating } from "@/shared/uiKit/rating";
 export const Main = () => {
   return (
     <>
-      <Heading size="large">Заголовок большой</Heading>
-      <Heading size="medium">Заголовок средний</Heading>
-      <Heading size="small">Заголовок маленький</Heading>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex...
-      </Paragraph>
-      <div style={{ display: "flex", gap: "10px", flexDirection: "column" }}>
+      <div style={{ display: "flex", gap: "9px", flexDirection: "column" }}>
+        <Heading size="large">Заголовок большой</Heading>
+        <Heading size="medium">Заголовок средний</Heading>
+        <Heading size="small">Заголовок маленький</Heading>
+        <Paragraph>
+          Блок текста <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex...
+        </Paragraph>
+
         {mockData.map(({ name, role, image }) => (
           <ActorCard key={name} name={name} role={role} image={image} />
         ))}
         <div>
-          <Rating rating={6} />
+          <Rating rating={10} />
+          <Rating rating={4.5} />
+          <Rating rating={3} />
         </div>
       </div>
-      <Input placeholder="Поле ввода" />
+      <Input placeholder="Поле ввода" setValue={() => {}} />
       <Button>Кнопка</Button>
     </>
   );
