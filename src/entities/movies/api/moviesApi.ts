c6f23b85a942eeec17741c09d8c21909b3a9c0e5
Serge <justsegaekb@yandex.ru>
2024-08-17 22:25:18 +0500

    fix bugs

--- src/entities/movies/api/moviesApi.ts
+++ src/entities/movies/api/moviesApi.ts
@@ -1,41 +1,36 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { MoviesApiResonse, ParamsType } from "../../../shared/types/types";
+import { IMovie, ParamsType } from "@/shared/types/types";
 import { setMovies } from "../model/moviesSlice";
 const BASE_URL = import.meta.env.VITE_KINOPOISK_BASE_URL;
 const API_KEY = import.meta.env.VITE_X_API_KEY;
 
 export const moviesApi = createApi({
-  reducerPath: 'moviesApi',
+  reducerPath: "moviesApi",
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
-    getMovies: builder.query<MoviesApiResonse, ParamsType>({
+    getMovies: builder.query<IMovie[], ParamsType>({
       query: (params) => {
-        const { 
-          page = 1, 
-          limit = 10, 
-          query = "",
-        } = params || {};
-        
+        const { page = 1, limit = 10, query = "" } = params || {};
+
         return {
-          url: 'movie/search',
+          url: "movie/search",
           headers: {
             "X-API-KEY": API_KEY,
           },
           params: {
-          page, 
-          limit,
-          query,
+            page,
+            limit,
+            query,
           },
-        }
+        };
       },
       async onQueryStarted(_arg, { dispatch, queryFulfilled }) {
         const result = await queryFulfilled;
         const data = result.data;
-        dispatch(setMovies(data.docs))
+        dispatch(setMovies(data));
       },
-    })
-  })
-})
+    }),
+  }),
+});
 
 export const { useGetMoviesQuery } = moviesApi;
-
